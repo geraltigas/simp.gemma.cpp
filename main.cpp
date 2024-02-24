75ee07d6c4a8bf69dde328053d6fff92e2a4fa2b
geraltigas <jb030112@outlook.com>
2024-02-24 22:56:05 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "This is simp.gemma.cpp, a LLM inference engine for my computer(intel cpu and gpu with unified memory)" << std::endl;
+    return 0;
+}
